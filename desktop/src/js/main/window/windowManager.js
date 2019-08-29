37022d47c8ab122cf5f507394c549ca767db9386
Borys Tymiński <tyminski.borys@gmail.com>
2019-08-29 20:34:28 +0200

    windowManager

---
+++ desktop/src/js/main/window/windowManager.js
@@ -0,0 +1,20 @@
+const { BrowserWindow } = require('electron');
+
+module.exports = windowManager = {
+    create(winUrl, size) {
+        const config = {
+            height: size.height,
+            width: size.width,
+            useContentSize: true,
+            webPreferences: {
+                nodeIntegration: true,
+                nodeIntegrationInWorker: false
+            }
+        };
+        let windowId = new BrowserWindow(config);
+        //windowId.setMenu(null);
+        windowId.loadURL(winUrl);
+        windowId.on('closed', () => windowId = null);
+        return windowId;
+    }
+};
