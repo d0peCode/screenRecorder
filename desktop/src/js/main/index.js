b41a01aec4d92cf621cbddaa25ac1f30642a9d64
Borys Tymiński <tyminski.borys@gmail.com>
2019-08-30 17:17:32 +0200

    refactor create window method

--- desktop/src/js/main/index.js
+++ desktop/src/js/main/index.js
@@ -6,7 +6,12 @@ const {dialog, app, ipcMain} = require('electron');
 let mainWindow, transparentWindow;
 
 app.on('ready', () => {
-    mainWindow = window.create(path.join(__dirname, '../..', 'html/index.html'), {width: 500, height: 400})
+    mainWindow = window.create(
+        path.join(__dirname, '../..', 'html/index.html'), 
+        {width: 500, height: 400}, 
+        [], 
+        [{name: 'setMenu', value: null}]
+    );
 });
 
 ipcMain.on('pick::path', async() => {
@@ -17,7 +22,21 @@ ipcMain.on('pick::path', async() => {
 ipcMain.on('start::record', async() => {
     mainWindow.minimize();
     const url = __dirname + '../../../html/index.html#!/recording';
-    transparentWindow = window.create(url, {width: 1000, height: 500});
+    transparentWindow = window.create(
+        url, 
+        {width: 1000, height: 500},
+        [
+            {name: 'transparent', value: true},
+            {name: 'frame', value: false},
+            {name: 'alwaysOnTop', value: true},
+        ],
+        [
+            {name: 'setMenu', value: null},
+            {name: 'setIgnoreMouseEvents', value: true},
+            {name: 'setFocusable', value: false},
+            {name: 'setFullScreen', value: true}
+        ]
+    );
 });
 
 ipcMain.on('stop::record', async() => {
--- desktop/src/js/main/window.js
+++ desktop/src/js/main/window.js
@@ -1,7 +1,7 @@
 const { BrowserWindow } = require('electron');
 
 module.exports = window = {
-    create(winUrl, size) {
+    create(winUrl, size, options, methods) {
         const config = {
             height: size.height,
             width: size.width,
@@ -11,8 +11,17 @@ module.exports = window = {
                 nodeIntegrationInWorker: false
             }
         };
+        if(config && config.length > 0) {
+            for(let i = 0; i < options.length; i++) {
+                config[options[i].name] = options[i].value
+            }
+        }
         let windowId = new BrowserWindow(config);
-        //windowId.setMenu(null);
+        if(methods && methods.length > 0) {
+            for(let i = 0; i < methods.length; i++) {
+                windowId[methods[i].name](methods[i].value);
+            }
+        }
         windowId.loadURL(winUrl);
         windowId.on('closed', () => windowId = null);
         return windowId;
